7ae51f53784898da935ad6086bdd23b951c9ec31
Kevin Talbert <ktalbert@cloudera.com>
2024-02-03 14:27:20 -0500

    Updated load pdfs process

--- 1_job-load-docs-to-pinecone/load-to-pinecone.py
+++ 1_job-load-docs-to-pinecone/load-to-pinecone.py
@@ -14,10 +14,18 @@ pinecone.init(
 openai_api_key = os.getenv("OPENAI_API_KEY")
 index_name = os.getenv("PINECONE_INDEX")
 
+embeddings = OpenAIEmbeddings(
+    openai_api_key=os.getenv("OPENAI_API_KEY")
+    # openai_api_base=OPENAI_API_BASE, 
+    # openai_api_type=OPENAI_API_TYPE, 
+    # openai_api_version=OPENAI_API_VERSION,
+    # chunk_size=1
+)
+
 # Check if the Pinecone index exists, if not, create it
 existing_indexes = pinecone.list_indexes()
 if index_name not in existing_indexes:
-    pinecone.create_index(index_name, dimension=768, metric="cosine")
+    pinecone.create_index(index_name, dimension=1536, metric="cosine")
     
 # Initialize Pinecone index object
 index = pinecone.Index(index_name)
@@ -29,10 +37,18 @@ folder_path = '/home/cdsw/docs'
 pdf_files = [file for file in os.listdir(folder_path) if file.endswith('.pdf')]
 
 for pdf_file in pdf_files:
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    print(pdf_file)
+    loader = PyPDFLoader(folder_path + "/" + pdf_file)
+    data = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     texts = text_splitter.split_documents(data)
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
     docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+    print("Loaded PDF document " + pdf_file + " successfully to Pinecone index " + index_name)
+    
+#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+#     texts = text_splitter.split_documents(data)
+#     embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+#     docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
 
 
 
